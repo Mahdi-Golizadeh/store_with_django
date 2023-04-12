bdf9352163fe197ccc76484c8d17d32ebe10e051
Mahdi-Golizadeh <mastergolizadeh@gmail.com>
2023-04-12 14:40:37 +0330

    in progress

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third-party
+    "crispy_forms", # new
+    "crispy_bootstrap5", # new
     #local: defininf the root to the config
     "accounts.apps.AccountsConfig",
     "pages.apps.PagesConfig", # new
@@ -128,3 +131,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "accounts.CustomUser"
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+# django-crispy-forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5" # new
+CRISPY_TEMPLATE_PACK = "bootstrap5" # new
